987e9c3b9b1e4aaf027574c53640dd67e9d9b0cb
Lukas Kuntze <privat@lukas-kuntze.de>
2021-05-21 20:43:36 +0200

    added DojoAssistant
     - Added NuGet Package FluentAssertions/5.10.3
     - Added DojoAssistantTests
     - Added DojoAssistant
     - Added DojoState

---
+++ Solutions/MediFox.DojoAssistant.Tests/DojoAssistantTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using MediFox.DojoAssistant.Enums;
+using Xunit;
+
+namespace MediFox.DojoAssistant.Tests
+{
+	public class DojoAssistantTests
+	{
+		[Fact]
+		public void Constructor_CreateNewDojoAssistant_DojoStateShouldBeIdle()
+		{
+			var dojoAssistant = new DojoAssistant(60);
+			var currentDojoState = dojoAssistant.DojoState;
+
+			currentDojoState.Should().Be(State.Idle);
+		}
+	}
+}
---
+++ Solutions/MediFox.DojoAssistant/DojoAssistant.cs
@@ -0,0 +1,17 @@
+﻿using MediFox.DojoAssistant.Enums;
+
+namespace MediFox.DojoAssistant
+{
+	public class DojoAssistant
+	{
+		private readonly int _roundTimeInSeconds;
+		
+		public State DojoState { get; private set; }
+		
+		public DojoAssistant(int roundTimeInSeconds)
+		{
+			_roundTimeInSeconds = roundTimeInSeconds;
+			DojoState = State.Idle;
+		}
+	}
+}
